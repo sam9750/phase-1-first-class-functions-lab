9be36f0531e341ba03c22a07adee4230c110995b
sam9750 <sam.wadhwa95@gmail.com>
2022-01-19 23:02:05 -0500

    commit tree 6252d29bf5a1183a23a01b07c4d36212f5538bda

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(arr){ 
+    console.log(arr);
+    return arr.slice (0,2)}
+
+
+
+const returnLastTwoDrivers = function(arr) {
+    return arr.slice (2,4)}
+
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// const one = function(newArr) {
+//     const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//     return newArr
+
+// }
+
+// function add (x,y) {
+//     return x + y;
+
+// }
+
+function createFareMultiplier(fareMultiplier) {
+    return function(fare) {
+        return fareMultiplier * fare
+
+    }
+}
+   
+   
+   
+//     function createFareMultiplier() {
+//         const fareMultiplier = Math.random()
+//         return function(fare) {
+//             return fareMultiplier * fare
+    
+//         }
+    
+//     
+    
+// }
+const fareDoubler = function(fare) {
+    return fare * 2 
+
+}
+const fareTripler = function(fare) {
+    return fare * 3
+}
+
+
+
+
+
+function selectDifferentDrivers(arrayOfDrivers, firstTwoOrLastTwo) {
+    return firstTwoOrLastTwo(arrayOfDrivers);
+}
+   
+//  const myArrayOfDrivers = []
+//  const returnFirstTwoDrivers = function(arr){ 
+//     console.log(arr);
+//     return arr.slice (0,2)}
+
+// selectDifferentDrivers(myArrayOfDrivers, returnFirstTwoDrivers) 
+
+
+//  firstTwoOrLastTwo(returnFirstTwoDrivers(), returnLastTwoDrivers());
+  
+// }
